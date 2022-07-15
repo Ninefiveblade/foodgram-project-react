073af3c61a5a86d9bf7380f2ae8f4f01b2b4487c
Ninefiveblade <cherep1.92@yandex.ru>
2022-07-15 14:45:27 +0300

    фильтры

--- backend/footgram/api/filters.py
+++ backend/footgram/api/filters.py
@@ -10,10 +10,13 @@ class RecipeFilter(filters.FilterSet):
 
     tags = filters.CharFilter(
         field_name="tags__slug",
-        lookup_expr="icontains"
+        method='filter_tags'
     )
     name = filters.CharFilter(field_name="name", lookup_expr="icontains")
     author = filters.CharFilter(field_name="author_id")
+
+    def filter_tags(self, queryset, slug, tags):
+        return queryset.filter(tags__slug__contains=tags.split(','))
 
     class Meta:
         model = Recipe
