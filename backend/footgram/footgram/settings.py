9c454ea74f3cda4e8a3e97b2cf452fbaf438c65d
Ninefiveblade <cherep1.92@yandex.ru>
2022-07-21 00:23:05 +0300

    Последнее ревью

--- backend/footgram/footgram/settings.py
+++ backend/footgram/footgram/settings.py
@@ -15,9 +15,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY') or get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(' ')
 
 
 # Application definition
